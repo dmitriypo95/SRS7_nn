a6be8165d91e60b7343f883c83ff0963b5f77bf5
Dmitriy Popinako <dmitriypo95@gmail.com>
2021-11-07 03:14:35 +0700

    create srs7

---
+++ app/src/main/java/com/dmitriypo95/srs7/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.dmitriypo95.srs7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
